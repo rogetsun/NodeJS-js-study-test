48e3339d1fbe0d4abbd20f08b20e427b81dc873d
rogetsun <songyw163@163.com>
2015-08-10 15:56:22 +0800

    node-usb  module, download from github

---
+++ mysql/in2.js
@@ -0,0 +1,10 @@
+/**
+ * Created by uv2sun on 15/7/31.
+ */
+var log = require('uvlog');
+var conn = require('./connect')();
+var seq = require('./sequence');
+//
+//seq.run(conn, 'seq1', 100000);
+seq.run(conn, 'seq2', 10000);
+//seq.run(conn, 'seq3', 100000);
