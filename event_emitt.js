8ee876a949bb9c5c637b105824f384e3ea12ab4a
rogetsun <songyw163@163.com>
2016-04-08 22:50:36 +0800

    emitter

--- event_emitt.js
+++ event_emitt.js
@@ -1,6 +1,10 @@
 var EventEmitter = require('events').EventEmitter;
 var ee = new EventEmitter();
-ee.on('join', function(){
-	console.log('welcome!');
+ee.on('join', function () {
+    console.log('welcome!');
+    console.log(arguments[0]);
+    console.log(arguments[1]);
 });
-setTimeout(function(){ee.emit('join');},2000);
+setTimeout(function () {
+    ee.emit('join', {a: 1}, 'you are pig!');
+}, 2000);
